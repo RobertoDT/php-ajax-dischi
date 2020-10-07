48dc29f65bc4517c0648d0cce44f228212e080cd
Roberto Del Toro <robertodeltoro93@gmail.com>
2020-10-07 17:22:35 +0200

    Prima versione completa dischi

--- db.php
+++ db.php
@@ -2,37 +2,37 @@
 
   $database = [
     [
-      'title' => 'New Jersey',
+      'title' => 'NEW JERSEY',
       'author' => 'Bon Jovi',
       'year' => 1988,
       'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
     ],
     [
-      'title' => 'Live at Wembley 86',
+      'title' => 'LIVE AT WEMBLEY 86',
       'author' => 'Queen',
       'year' => 1992,
       'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg'
     ],
     [
-      'title' => 'Ten\'s Summoner\'s Tales',
+      'title' => 'TEN\'S SUMMONE\'S TALES',
       'author' => 'Sting',
       'year' => 1993,
       'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg'
     ],
     [
-      'title' => 'Steve Gadd band',
+      'title' => 'STEVE GADD BAND',
       'author' => 'Steve Gadd Band',
       'year' => 2018,
       'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg'
     ],
     [
-      'title' => 'Brave new World',
+      'title' => 'BRAVE NEW WORLD',
       'author' => 'Iron Maiden',
       'year' => 2000,
-      'poster' => 'https://i.ebayimg.com/images/g/1GwAAOSwFytaKr7l/s-l300.jpg'
+      'poster' => 'https://i.ebayimg.com/images/g/PRYAAOSwBBRch6rW/s-l300.jpg'
     ],
     [
-      'title' => 'One more car, one more rider',
+      'title' => 'ONE MORE CAR, ONE MORE RAIDER',
       'author' => 'Eric Clapton',
       'year' => 2002,
       'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg'
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="dist/style.css">
     <title>Dischi</title>
   </head>
@@ -21,7 +22,7 @@
 
           <?php foreach ($database as $discs) { ?>
             <div class="card">
-              <img class="disc-img" src="<?php echo $discs["poster"]; ?>" alt="">
+              <img class="disc-img" src="<?php echo $discs["poster"]; ?>" alt="img-dischi">
               <h2 class="disc-title"><?php echo $discs["title"]; ?></h2>
               <h4 class="disc-author"><?php echo $discs["author"]; ?></h4>
               <p class="year"><?php echo $discs["year"]; ?></p>
