197f0ada516cf8ae3a41d3c55214007a58d412e9
Roberto Del Toro <robertodeltoro93@gmail.com>
2020-10-07 16:33:22 +0200

    Aggiunti file .scss. Db stampato, manca la resa grafica

--- index.php
+++ index.php
@@ -1,42 +1,37 @@
-<?php
-​
-  $database = [
-    [
-      'title' => 'New Jersey',
-      'author' => 'Bon Jovi',
-      'year' => 1988,
-      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
-    ],
-    [
-      'title' => 'Live at Wembley 86',
-      'author' => 'Queen',
-      'year' => 1992,
-      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg'
-    ],
-    [
-      'title' => 'Ten\'s Summoner\'s Tales',
-      'author' => 'Sting',
-      'year' => 1993,
-      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg'
-    ],
-    [
-      'title' => 'Steve Gadd band',
-      'author' => 'Steve Gadd Band',
-      'year' => 2018,
-      'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg'
-    ],
-    [
-      'title' => 'Brave new World',
-      'author' => 'Iron Maiden',
-      'year' => 2000,
-      'poster' => 'https://i.ebayimg.com/images/g/1GwAAOSwFytaKr7l/s-l300.jpg'
-    ],
-    [
-      'title' => 'One more car, one more rider',
-      'author' => 'Eric Clapton',
-      'year' => 2002,
-      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg'
-    ]
-  ];
-​
-?>
+<?php include "db.php" ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="dist/style.css">
+    <title>Dischi</title>
+  </head>
+  <body>
+
+    <header>
+      <div class="wrapper">
+        <img class="logo-spoti" src="img/logo_spotify.png" alt="logo_spotify">
+      </div>
+    </header>
+
+    <main>
+      <div class="wrapper">
+        <div class="cards">
+
+          <?php foreach ($database as $discs) { ?>
+            <div class="card">
+              <img class="disc-img" src="<?php echo $discs["poster"]; ?>" alt="">
+              <h2 class="disc-title"><?php echo $discs["title"]; ?></h2>
+              <h4 class="disc-author"><?php echo $discs["author"]; ?></h4>
+              <p class="year"><?php echo $discs["year"]; ?></p>
+            </div>
+          <?php } ?>
+
+        </div>
+      </div>
+    </main>
+
+  <script src="dist/main.js" charset="utf-8"></script>
+  </body>
+</html>
